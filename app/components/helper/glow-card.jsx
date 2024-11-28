abdaafc1c6abb883a339d554915d8869ab1493bd
Andres Garcia Sanchez <force.rap20@icloud.com>
2024-11-27 19:33:36 -0600

    fixing server side error

--- app/components/helper/glow-card.jsx
+++ app/components/helper/glow-card.jsx
@@ -1,8 +1,12 @@
-"use client"
-import { useEffect } from 'react';
+"use client";
 
-const GlowCard = ({ children , identifier}) => {
+import { useEffect } from "react";
+
+const GlowCard = ({ children, identifier }) => {
   useEffect(() => {
+    // Ensure this code runs only in the browser
+    if (typeof document === "undefined") return;
+
     const CONTAINER = document.querySelector(`.glow-container-${identifier}`);
     const CARDS = document.querySelectorAll(`.glow-card-${identifier}`);
 
@@ -25,9 +29,9 @@ const GlowCard = ({ children , identifier}) => {
           event?.y > CARD_BOUNDS.top - CONFIG.proximity &&
           event?.y < CARD_BOUNDS.top + CARD_BOUNDS.height + CONFIG.proximity
         ) {
-          CARD.style.setProperty('--active', 1);
+          CARD.style.setProperty("--active", 1);
         } else {
-          CARD.style.setProperty('--active', CONFIG.opacity);
+          CARD.style.setProperty("--active", CONFIG.opacity);
         }
 
         const CARD_CENTER = [
@@ -42,28 +46,28 @@ const GlowCard = ({ children , identifier}) => {
 
         ANGLE = ANGLE < 0 ? ANGLE + 360 : ANGLE;
 
-        CARD.style.setProperty('--start', ANGLE + 90);
+        CARD.style.setProperty("--start", ANGLE + 90);
       }
     };
 
-    document.body.addEventListener('pointermove', UPDATE);
-
     const RESTYLE = () => {
-      CONTAINER.style.setProperty('--gap', CONFIG.gap);
-      CONTAINER.style.setProperty('--blur', CONFIG.blur);
-      CONTAINER.style.setProperty('--spread', CONFIG.spread);
+      CONTAINER.style.setProperty("--gap", CONFIG.gap);
+      CONTAINER.style.setProperty("--blur", CONFIG.blur);
+      CONTAINER.style.setProperty("--spread", CONFIG.spread);
       CONTAINER.style.setProperty(
-        '--direction',
-        CONFIG.vertical ? 'column' : 'row'
+        "--direction",
+        CONFIG.vertical ? "column" : "row"
       );
     };
+
+    document.body.addEventListener("pointermove", UPDATE);
 
     RESTYLE();
     UPDATE();
 
     // Cleanup event listener
     return () => {
-      document.body.removeEventListener('pointermove', UPDATE);
+      document.body.removeEventListener("pointermove", UPDATE);
     };
   }, [identifier]);
 
