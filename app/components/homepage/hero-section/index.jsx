addbe9645341a4c4e6d8a58da63b74f6086efdbe
Andres Garcia Sanchez <force.rap20@icloud.com>
2025-05-19 17:54:23 -0500

    updated skills

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -131,6 +131,12 @@ function HeroSection() {
                 <span className="text-amber-300">Docker</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">AWS</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Terraform</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Langchain</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Pinecone</span>
                 <span className="text-gray-400">{"'],"}</span>
               </div>
               <div>
